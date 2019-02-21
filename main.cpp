7cca91da4eccfa5023d7a5ec76c111e2430d4b06
BT228 <svc_OKC_BT228@ad.osuokc.edu>
2019-02-20 19:24:31 -0600

    for loop

--- main.cpp
+++ main.cpp
@@ -1,21 +1,38 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 // CIS2013_Week05_Lab1
 
+	
+const int MAX = 30;
+int student[MAX];
+string name[MAX];
+
+void makeStudent(int x){
+	cout << "Enter Student " << x+1 << "'s name ";
+	cin >> name[x];
+	cout << "Enter " << name[x] << "'s grade: ";
+	cin >> student[x];
+}
+
+void printGrade(int y){
+	cout << name[y] << "'s grade is " << student [y] << endl;
+}
 int main(){
+
+	int LEN;
+
 	
-	int LEN = 2;
-	
-	int student[LEN];
+	cout << "How many students are in this class: ";
+	cin >> LEN;
 	
 	for(int i=0; i<LEN; i++){
-		cout << "Enter Student " << i+1 << "'s grade ";
-		cin >> student[i];
+		makeStudent(i);
 	}
 	
 	for (int j=0; j<LEN; j++){
-		cout << "Student " << j+1 << "'s grade is " << student [j] << endl;
+		printGrade(j);
 	}
 	
 	return 0;
